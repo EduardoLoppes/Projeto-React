bd743feb2c8080a7264d4f7439ab4e11c0aa7510
EduardoLoppes <mcfgonzalles@gmail.com>
2023-04-18 17:43:06 -0300

    autalizando a tela de login e adicionando menu.js

--- components/Login/login.js
+++ components/Login/login.js
@@ -1,61 +1,62 @@
-import  React, {useState} from 'react';
-import{StyleSheet, View, Text, Image, SafeAreaView, TouchableOpacity } from 'react-native';
-  import{TextInput} from 'react-native-paper';
-  import firebase from '../../assets/services/connectionFirebase';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Image, SafeAreaView, TouchableOpacity, handleLogin } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import firebase from '../../assets/services/connectionFirebase';
 const Separator = () => <View style={styles.separator} />;
-export default function Login({changeStatus}) {
-const [email, setEmail] = useState('');
+export default function Login({ changeStatus }) {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [type, setType] = useState('login');
 
-    return (
-      <View style={styles.container}>
-       <Image  source={require('../../assets/usuarioimg.png')}
-       style={{width: 420, height:340}} />
-           <Separator/>
-        <SafeAreaView> 
-        
-       <TextInput
-       
-      label="Email"
- style={styles.input}
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../assets/usuarioimg.png')}
+        style={{ width: 420, height: 340 }} />
+      <Separator />
+      <SafeAreaView>
+
+        <TextInput
+
+          label="Email"
+          style={styles.input}
           value={email}
           onChangeText={(text) => setEmail(text)}
-      right={<TextInput.Icon icon="email" />}
-    />
-    <TextInput
-      label="Senha"
-       style={styles.input}
+          right={<TextInput.Icon icon="email" />}
+        />
+        <TextInput
+          label="Senha"
+          style={styles.input}
           value={password}
           onChangeText={(text) => setPassword(text)}
-      secureTextEntry
-      right={<TextInput.Icon icon="eye" />}
-    />
-       </SafeAreaView>
-       <TouchableOpacity
-                style={[styles.handleLogin,
-                { backgroundColor: type === 'login' ? '#4682B4' : '#141414' }]}
-                onPress={'handleLogin'}
-                //para verificar se é login ou cadastro
-            >
-                <Text style={styles.loginText}>
-                    {type === 'login' ? 'Acessar' : 'Cadastrar'}
-                </Text>
-            </TouchableOpacity>
+          secureTextEntry
+          right={<TextInput.Icon icon="eye" />}
+        />
+      </SafeAreaView>
 
- 
+      <TouchableOpacity
+        style={[styles.handleLogin,
+        { backgroundColor: type === 'login' ? '#4682B4' : '#141414' }]}
+        onPress={handleLogin}
+      //para verificar se é login ou cadastro
+      >
+        <Text style={styles.loginText}>
+          {type === 'login' ? 'Acessar' : 'Cadastrar'}
+        </Text>
+      </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => setType(type => type === 'login' ? 'cadastrar' : 'login')} >
-                <Text style={{ textAlign: 'center' }}>
-                    {type === 'login' ? 'Criar uma conta' : 'Já possuo uma conta'}
-                </Text>
-            </TouchableOpacity>
-      </View>
-    );
-   }
-   
-  const styles = StyleSheet.create({
-     input: {
+
+
+      <TouchableOpacity onPress={() => setType(type => type === 'login' ? 'cadastrar' : 'login')} >
+        <Text style={{ textAlign: 'center' }}>
+          {type === 'login' ? 'Criar uma conta' : 'Já possuo uma conta'}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
     marginBottom: 20,
     marginLeft: 40,
     backgroundColor: '#FFF',
@@ -66,11 +67,29 @@ const [email, setEmail] = useState('');
     borderWidth: 1,
     borderColor: '#141414'
   },
-loginText:{
-textAlign: 'center'
-},
-separator: {
-  marginVertical: 20,
-}
-  });
- 
+  handleLogin: {
+
+    alignItems: 'center',
+
+    justifyContent: 'center',
+
+    height: 45,
+
+    marginTop: 30,
+
+  },
+
+  loginText: {
+
+    color: '#FFF',
+
+    fontSize: 24,
+
+  },
+  loginText: {
+    textAlign: 'center'
+  },
+  separator: {
+    marginVertical: 20,
+  }
+});
